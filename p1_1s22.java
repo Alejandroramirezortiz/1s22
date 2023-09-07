1972c76457b2651e30000608a9cb26c5b38c3c2e
alexkiller55 <144191160+alexkiller55@users.noreply.github.com>
2023-09-06 18:53:38 -0600

    Add files via upload
    alejandro ramirez ortiz 1s22 practica uno y dos

---
+++ p1_1s22.java
@@ -0,0 +1,4 @@
+class p1_1s22{public static void main(String[] args) {
+	System.out.println("hola, mundo");
+	System.out.println("estamos empezando");
+}} 
---
+++ p2_1s22.java
@@ -0,0 +1,7 @@
+class p2_1s22 {public static void main(String[] args) {
+	System.out.println("nombre:alejandro ramirez ortiz");
+	System.out.println("edad:18 años");
+	System.out.println("correo:alexramiorti10@gmail.com");
+	System.out.println("aficion:jugarfutbol y trabajar");
+	System.out.println("color favorito:azul");
+}}
