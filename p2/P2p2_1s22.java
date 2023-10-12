c1baeeb0d5f36bd9d5d3337b30b7d045c1982d14
Alejandro Ramirez Ortiz <144191160+Alejandroramirezortiz@users.noreply.github.com>
2023-10-11 19:42:38 -0600

    Add files via upload

---
+++ p2/P2p2_1s22.java
@@ -0,0 +1,33 @@
+public class P2p2_1s22{
+	public static void main(String[] args) {
+		int a;
+		for(a=0;a<10;a++){
+			System.out.println("esto se repite"+a);
+		}
+		a=0;
+		for(;a>10;a+=2){//a=a+2
+			System.out.println("estos son los valores"+a);
+
+
+
+		}
+
+		a=1;
+		for(;;a++){
+			System.out.println("ahora sin 2 elementos " + a);
+			if (a>10) break;
+			a++;{
+
+                int b=2,c=10;
+
+				for (a=2;a<101;a++){
+					System.out.println(""+ a);
+					 b++;
+					if(b==10){
+						b=1;
+						System.out.println();
+					 b++;
+				}
+			}
+		}
+	}
